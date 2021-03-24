69f74efef4fc8ce2e965ac4060d13bd5434d5b90
Mauricio Bernardes <mauricio.bqs@gmail.com>
2021-03-24 12:24:30 -0300

    fibonacci

---
+++ Fibonacci/Fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main() {
+
+    int a, b, auxiliar, i, n;
+
+    a = 0;
+    b = 1;
+
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    printf("\nSérie de Fibonacci:\n\n");
+    printf("%d\n", b);
+
+    for(i = 0; i < n; i++) {
+
+        auxiliar = a + b;
+        a = b;
+        b = auxiliar;
+
+        printf("%d\n", auxiliar);
+    }
+}
